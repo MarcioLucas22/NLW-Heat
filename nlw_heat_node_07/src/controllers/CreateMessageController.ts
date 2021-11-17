1ee207500e8da67c9e23616865aed083a531498f
Márcio Lucas <marciolucasmachado22@gmail.com>
2021-11-17 14:56:39 -0300

    NLW Heat

---
+++ nlw_heat_node_07/src/controllers/CreateMessageController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import { CreateMessageService } from "../services/CreateMessageService";
+
+class CreateMessageController {
+
+  async handle(req: Request, res: Response) {
+    const { message } = req.body
+    const { user_id } = req
+
+    const createMessageService = new CreateMessageService()
+
+    const messages = await createMessageService.execute(message, user_id)
+
+    return res.send(messages)
+  }
+}
+
+export { CreateMessageController }
